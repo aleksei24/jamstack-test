e19f3d1388475173c6852f9195bf2c3412544d69
aleksei24 <lexa24032@gmail.com>
2021-04-27 16:33:18 +0300

    api-key has been stashed

---
+++ src/_data/news.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+module.exports = async function getUser() {
+    try {
+        const response = await axios.get(
+            `https://newsapi.org/v2/top-headlines?country=ru&apiKey=${process.env.API_KEY}`
+        );
+        return response.data;
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+};
