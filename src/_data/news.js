322e7e089d933c881de42f03b0f2447468ce1b9a
aleksei24 <lexa24032@gmail.com>
2021-04-28 17:02:16 +0300

    news is displayed

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,22 @@
 const axios = require('axios');
+const countries = require('./countries.json');
 require('dotenv').config();
 
-module.exports = async function () {
+async function getNews(country) {
     try {
         const response = await axios.get(
-            `https://newsapi.org/v2/top-headlines?country=ru&apiKey=${process.env.API_KEY}`
+            `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}`
         );
-        return response.data;
+        return { country: country, articles: response.data.articles };
     } catch (error) {
         console.error(error);
     }
+}
+
+module.exports = async function () {
+    const newsPromise = countries.map(getNews);
+    return Promise.all(newsPromise).then((newsObjects) => {
+        console.log('News Objects:', newsObjects);
+        return [].concat.apply([], newsObjects);
+    });
 };
