09c6bef6be5775d12406dfe8f3e1910a619b1e24
aleksei24 <lexa24032@gmail.com>
2021-04-28 16:25:00 +0300

    news block is displayed

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,12 @@
 const axios = require('axios');
 require('dotenv').config();
 
-module.exports = async function getUser() {
+module.exports = async function () {
     try {
         const response = await axios.get(
             `https://newsapi.org/v2/top-headlines?country=ru&apiKey=${process.env.API_KEY}`
         );
         return response.data;
-        console.log(response);
     } catch (error) {
         console.error(error);
     }
